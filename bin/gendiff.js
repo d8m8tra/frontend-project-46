d341f9d4322690485b6599acbd4c80d9be515533
ki <kre1238@gmail.com>
2025-10-21 23:14:00 +0300

    upd gendif

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,4 +8,5 @@ program // инициализировали программу
     .name('genDiff') // объявили имя CLI-команды
     .description('Compares two configuration files and shows a difference') // задали описание
     .version('1.0.0') // задали версию
+    .option('-f, --format [type]', 'output format')
 program.parse()
