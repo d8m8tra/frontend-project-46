437295b4c95508192491711506ba170c6bfce850
ki <kre1238@gmail.com>
2025-10-22 16:23:51 +0300

    add arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,11 @@ import { Command } from 'commander'
 const program = new Command();
 
 program // инициализировали программу
-    .name('genDiff') // объявили имя CLI-команды
+    .name('gendiff') // объявили имя CLI-команды
     .description('Compares two configuration files and shows a difference') // задали описание
     .version('1.0.0') // задали версию
-    .option('-f, --format [type]', 'output format')
+    .option('-f, --format [type]', 'output format') // добавили опцию
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+
 program.parse()
